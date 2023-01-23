79f463a6ea212e5c5c1d97f8f133609da7e1e996
tLLWtG <tllwtg@gmail.com>
2023-01-23 15:03:06 +0800

    Fail: "DOMException: Not supported"

--- renderer.js
+++ renderer.js
@@ -5,3 +5,35 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const btn = document.getElementById("btn");
+const video = document.getElementById("video");
+
+btn.addEventListener("click", async function () {
+  const stream = await navigator.mediaDevices.getDisplayMedia({
+    video: true,
+  });
+  video.srcObject = stream;
+  video.onloadedmetadata = () => video.play();
+  const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
+    ? "video/webm; codecs=vp9"
+    : "video/webm";
+  const mediaRecorder = new MediaRecorder(stream, {
+    mimeType: mime,
+  });
+  //record
+  const chunks = [];
+  mediaRecorder.addEventListener("dataavailable", function (e) {
+    chunks.push(e.data);
+  });
+  //stop
+  mediaRecorder.addEventListener("stop", function () {
+    const blob = new Blob(chunks, { type: chunks[0].type });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "video.webm";
+    a.click();
+  });
+  //start manually
+  mediaRecorder.start();
+});
